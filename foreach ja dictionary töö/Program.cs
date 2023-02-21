34344d6a18fd675f3304742d0ee428879369ee41
opilane <opilane@HP-CZC0359NN5>
2023-02-21 09:12:26 +0200

    Add project files.

---
+++ foreach ja dictionary töö/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace foreach_ja_dictionary_töö
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var school = new Dictionary<int, string>
+            {
+                {1,"Ristiku_Põhikool"},
+                {2,"Tallinna_Kunstigümnaasium"},
+                {3,"Tallinna_Tööstushariduskeskus"}
+            };
+
+            foreach (var paar in school)
+            {
+                Console.WriteLine($"{paar.Key}. {paar.Value}");
+            }
+        }
+    }
+}
